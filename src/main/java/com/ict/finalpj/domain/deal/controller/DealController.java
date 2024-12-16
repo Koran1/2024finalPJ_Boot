0af8d4f4be4332017136d79c076d2448d845a257
Koran1 <thekorani@gmail.com>
2024-12-16 11:10:27 +0900

    241216 파일 초기설정 오류 수정

--- src/main/java/com/ict/finalpj/domain/deal/controller/DealController.java
+++ src/main/java/com/ict/finalpj/domain/deal/controller/DealController.java
@@ -1,5 +1,6 @@
 package com.ict.finalpj.domain.deal.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- src/main/java/com/ict/finalpj/domain/deal/mapper/DealMapper.java
+++ src/main/java/com/ict/finalpj/domain/deal/mapper/DealMapper.java
@@ -1,5 +1,7 @@
 package com.ict.finalpj.domain.deal.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
 @Mapper
 public interface DealMapper {
 
