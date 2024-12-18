7f3f2412ed88329c6e599fc027ccfb288bbff4e3
davidgw <coreax7@gmail.com>
2024-12-18 17:46:39 +0900

    241218 로그인 풀

--- src/main/java/com/ict/finalpj/domain/camp/controller/CampController2.java
+++ src/main/java/com/ict/finalpj/domain/camp/controller/CampController2.java
@@ -1,6 +1,7 @@
 package com.ict.finalpj.domain.camp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @RestController
@@ -33,6 +35,24 @@ public class CampController2 {
             dataVO.setSuccess(true);
             dataVO.setMessage("캠핑장 상세보기 실패");
             log.info("캠핑장 상세보기 실패");
+        }
+        return dataVO;
+    }
+
+    @GetMapping("/fav/{campIdx}")
+    public DataVO getFavCamp(@RequestParam String campIdx, Authentication authentication) {
+        DataVO dataVO = new DataVO();
+        try {
+            // 로그인 여부 확인
+            if (authentication == null) {
+                dataVO.setSuccess(false);
+                dataVO.setMessage("로그인이 필요합니다.");
+                return dataVO;
+            }
+            // int result = campService2.getFavCamp(campIdx);
+        } catch (Exception e) {
+            dataVO.setSuccess(false);
+            dataVO.setMessage("게스트북 삭제 오류 발생");
         }
         return dataVO;
     }
