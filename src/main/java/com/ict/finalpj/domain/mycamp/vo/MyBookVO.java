ddc47f6c223e98e9aed053e1f6717e53647a0f3a
이의진 <kouy96@naver.com>
2025-01-03 21:25:54 +0900

    250103 나의 예약 리스트 완성
    나의 예약 리스트 완성했으나 언동된 계획은 주말에 완성할 예정

---
+++ src/main/java/com/ict/finalpj/domain/mycamp/vo/MyBookVO.java
@@ -0,0 +1,44 @@
+package com.ict.finalpj.domain.mycamp.vo;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyBookVO {
+    private String 
+        bookIdx, // 예약 고유번호
+        campIdx, // 캠핑장 고유번호
+        facltNm, // 캠핑장명
+        firstImageUrl, // 캠핑장 대표사진
+        campImg2, // 캠핑장 두 번째 사진
+        userIdx, // 사용자 고유번호
+        // bookCheckInDate, // 예약기간 시작날짜
+        // bookCheckOutDate, // 예약기간 끝날짜
+        bookSelectedZone, // 선택된 구역
+        // bookAdultCount, // 성인 인원 수
+        // bookYouthCount, // 청소년 인원 수
+        // bookChildCount, // 미취학 아동 인원 수
+        // bookCarCount, // 예약 차량 수
+        planIdx, // 계획 글 고유번호
+        planBeginDate, // 캠핑 출발 날짜
+        planEndDate, // 캠핑 끝 날짜
+        planTitle, // 계획 제목
+        planRegDate, // 계획 작성일
+        planUpdateDate // 계획 수정일
+        ;
+
+    private int 
+        bookAdultCount, // 성인 인원 수
+        bookYouthCount, // 청소년 인원 수
+        bookChildCount, // 미취학 아동 인원 수
+        bookCarCount // 예약 차량 수
+    ;
+
+    private LocalDate bookCheckInDate; // 예약기간 시작날짜
+    private LocalDate bookCheckOutDate; // 예약기간 끝날짜
+}
